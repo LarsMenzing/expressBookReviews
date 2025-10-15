b8ff293e5537ad3c17d40327f46599ad1760f7cd
LarsMenzing <lars@menzing.com>
2025-10-15 10:43:23 -0400

    updated until task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,70 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;  // This will be a string like "1", "2", etc.
+  const book = books[isbn];      // Access the book using the key
+  
+  if (book) {
+    res.send(book);              // Send the book details if found
+  } else {
+    res.status(404).send({message: "Book not found"});
+  }
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const matchingBooks = [];
+
+  // Iterate over all books
+  for (const key in books) {
+    if (books[key].author.toLowerCase() === author.toLowerCase()) {
+      matchingBooks.push(books[key]);
+    }
+  }
+
+  if (matchingBooks.length > 0) {
+    res.send(matchingBooks);
+  } else {
+    res.status(404).send({ message: "No books found for the author" });
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const matchingBooks = [];
+
+  // Iterate over all books
+  for (const key in books) {
+    if (books[key].title.toLowerCase() === title.toLowerCase()) {
+      matchingBooks.push(books[key]);
+    }
+  }
+
+  if (matchingBooks.length > 0) {
+    res.send(matchingBooks);
+  } else {
+    res.status(404).send({ message: "No books found for the author" });
+  }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  
+  if (book) {
+    res.send(book.reviews);  // Send the reviews object for the book
+  } else {
+    res.status(404).send({ message: "Book not found" });
+  }
 });
+
 
 module.exports.general = public_users;
